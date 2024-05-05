e29ce0844769359a2aeb79429e7c6c31b1a57fc8
KoushikReddyB <2200090018@kluniversity.in>
2024-05-05 08:45:44 +0530

    AES Algorithm added

---
+++ SecureText/src/app/encryption/symmetric/__init__.py
@@ -0,0 +1,5 @@
+from . import aes
+
+DEFAULT_KEY_SIZE = 16
+
+__all__ = ['aes_encrypt']
---
+++ SecureText/tests/encryption/symmetric/aes_test_encryption.py
@@ -0,0 +1,14 @@
+# tests/encryption/symmetric/aes_test_encryption.py
+from src.app.encryption.symmetric import aes as aes_encrypt
+from src.app.decryption.symmetric import aes as aes_decrypt
+from Crypto.Random import get_random_bytes
+
+def test_aes_encryption():
+    message = "Hello, AES!"
+    key = get_random_bytes(16)  # 16 bytes for AES-128
+
+    ciphertext, iv = aes_encrypt.encrypt_message(message, key)
+    decrypted_message = aes_decrypt.decrypt_message(ciphertext, key, iv)
+
+    assert decrypted_message == message
+    print("Encryption and decryption test passed!")
