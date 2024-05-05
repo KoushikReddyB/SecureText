0e3ba72112d399ff48305594b5c62f201bae8614
KoushikReddyB <2200090018@kluniversity.in>
2024-05-05 09:14:08 +0530

    modified the test files

--- SecureText/tests/triple_des_test.py
+++ SecureText/tests/triple_des_test.py
@@ -2,12 +2,18 @@ from src.app.encryption.symmetric import triple_des as triple_des_encrypt
 from src.app.decryption.symmetric import triple_des as triple_des_decrypt
 from Crypto.Random import get_random_bytes
 
-def test_aes_encryption():
+def test_triple_des():
     message = "Hello, 3DES!"
     key = get_random_bytes(16)  # 16 bytes for AES-128
 
     ciphertext, iv = triple_des_encrypt.encrypt_message(message, key)
     decrypted_message = triple_des_decrypt.decrypt_message(ciphertext, key, iv)
 
+    print("Original message:", message)
+    print("Encrypted message:", ciphertext)
+    print("Decrypted message:", decrypted_message)
+    
     assert decrypted_message == message
     print("Encryption and decryption test passed!")
+
+test_triple_des()
